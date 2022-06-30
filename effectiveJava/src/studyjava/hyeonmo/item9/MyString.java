372d1842784fa392ee8c73839203f314e2d9dcd6
정현모 <gusah009@naver.com>
2022-06-30 11:27:48 +0900

    item9. try-finally 보다는 try-with-resources를 사용하라

---
+++ effectiveJava/src/studyjava/hyeonmo/item9/MyString.java
@@ -0,0 +1,41 @@
+package studyjava.hyeonmo.item9;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MyString {
+
+  static final int BUFFER_SIZE = 100;
+
+  static void copy(String src, String dst) throws IOException {
+    InputStream in = new FileInputStream(src);
+    try {
+      OutputStream out = new FileOutputStream(dst);
+      try {
+        byte[] buf = new byte[BUFFER_SIZE];
+        int n;
+        while ((n = in.read(buf)) >= 0) {
+          out.write(buf, 0, n);
+        }
+      } finally {
+        out.close();
+      }
+    } finally {
+      in.close();
+    }
+  }
+
+  static void advanceCopy(String src, String dst) throws IOException {
+    try (InputStream in = new FileInputStream(src);
+        OutputStream out = new FileOutputStream(dst)) {
+      byte[] buf = new byte[BUFFER_SIZE];
+      int n;
+      while ((n = in.read(buf)) >= 0) {
+        out.write(buf, 0, n);
+      }
+    }
+  }
+}
