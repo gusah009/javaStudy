f11db78df0c5cccf3ab09d1c1d447afab0f53400
정핸모 <gusah009@naver.com>
2022-09-13 22:27:50 +0900

    webBlog: [스케줄러와 transaction]: 가장 무식한 방법

--- webBlog/src/test/java/com/example/webblog/service/MemberServiceTest.java
+++ webBlog/src/test/java/com/example/webblog/service/MemberServiceTest.java
@@ -1,7 +1,6 @@
 package com.example.webblog.service;
 
 import static com.example.webblog.repository.Team.DEFAULT_NAME;
-import static java.time.LocalDateTime.now;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.webblog.repository.Member;
@@ -12,19 +11,12 @@ import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 import javax.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionTemplate;
 
 @SpringBootTest
-@Transactional
+//@Transactional
 class MemberServiceTest {
 
   @Autowired
@@ -39,21 +31,6 @@ class MemberServiceTest {
   @Autowired
   EntityManager em;
 
-  @TestConfiguration
-  @RequiredArgsConstructor
-  static class TestSchedulerConfig {
-
-    private final PlatformTransactionManager txManager;
-
-    @Bean
-    TransactionTemplate schedulerTransactionTemplateTest() {
-      TransactionTemplate txTemplate = new TransactionTemplate(txManager);
-      txTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_UNCOMMITTED);
-      txTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_MANDATORY);
-      return txTemplate;
-    }
-  }
-
   @Test
   public void reserveMemberTeamChange() throws InterruptedException {
     // given
@@ -61,10 +38,6 @@ class MemberServiceTest {
     Team changeTeam = generateTeam("정현모");
     generateMember();
     generateMember();
-
-    // 쿼리가 나가야 새로운 스레드의 트랜잭션에서 데이터를 읽을 수 있다.
-    em.flush();
-    em.clear();
 
     // when
     String result = memberService.reserveMemberTeamChange(1);
@@ -75,10 +48,20 @@ class MemberServiceTest {
     // then
     assertThat(result).isEqualTo("SUCCESS");
     List<Member> resultMembers = memberRepository.findAll();
-    System.out.println("resultMembers = " + resultMembers);
-    Thread.sleep(2000);
-//    assertThat(resultMembers.get(0).getTeam()).isEqualTo(changeTeam);
-//    assertThat(resultMembers.get(1).getTeam()).isEqualTo(changeTeam);
+    assertThat(resultMembers.get(0).getTeam().getId()).isEqualTo(changeTeam.getId());
+    assertThat(resultMembers.get(1).getTeam().getId()).isEqualTo(changeTeam.getId());
+
+    // after
+    deleteAllMember();
+    deleteAllTeam();
+  }
+
+  private void deleteAllMember() {
+    memberRepository.deleteAll();
+  }
+
+  private void deleteAllTeam() {
+    teamRepository.deleteAll();
   }
 
   private Team generateTeam(String teamName) {
